2e82d13ece0d1970d18c9e98f2a9beae0bd7b42b
Angel Zúñiga Navarro <angel.zuna@icloud.com>
2023-01-21 19:29:34 -0500

    updated

--- .config/powershell/magenz.ps1
+++ .config/powershell/magenz.ps1
@@ -1,4 +1,4 @@
 #Initial configuration-----------------------------
-oh-my-posh init pwsh --config "$env:USERPROFILE\.config\Oh-My-Posh-theme\magen.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:USERPROFILE\.config\Oh-My-Posh-theme\magenz.omp.json" | Invoke-Expression
 Import-Module -Name Terminal-Icons
 set-PSReadLineOption -PredictionViewStyle ListView
